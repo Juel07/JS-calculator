b1991feb73b4acdb620886058c969f43b5bc55e4
Juel B <juel.braganza97@gmail.com>
2020-11-14 23:12:46 +0000

    select all buttons to manipulate in the JS

---
+++ script.js
@@ -0,0 +1,12 @@
+// select the buttons
+const numberButtons = document.querySelectorAll("[data-number]")
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-prev-operand]')
+const currentOperandTextElement = document.querySelector('[data-curr-operand]')
+
+//how to store all the elements for the calc? use a class, that will take all inputs and functions for the
+//calculator
+
