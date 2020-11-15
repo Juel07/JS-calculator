d06d03352e5a18ec4f4ce21b39a2a6736cc9b6f6
Juel B <juel.braganza97@gmail.com>
2020-11-15 23:55:52 +0000

    defined and called the clear function in calculator class

--- script.js
+++ script.js
@@ -15,11 +15,18 @@ class Calculator {
         // set variables for this class
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        //call this function as soon as we create the calculator
+        this.clear()
     }
 
     // define all operations for our calculator class
-    clear() {
 
+    clear() {
+        //defin the following variables; clear the inputs and set them to default values; 
+        this.currentOperand = ""
+        this.previousOperand = ""
+        //operation is undefined since they don't have any operation selected if we clear things
+        this.operation = undefined
     }
 
     delete() {
